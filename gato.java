c0601d3a5bfc0a801d7e6f4b29ccaaeecd6ddf21
Gabriel Macedo <48810738+eugabrielmacedo@users.noreply.github.com>
2019-06-06 10:05:14 -0300

    Gato.java
    primeira parte do código em java do gato e parque na aula do dia 06/06

---
+++ gato.java
@@ -0,0 +1,24 @@
+package gatos;
+
+public class gato {
+	
+	
+	public class gato {
+       registro;
+       string nome;
+       raca;
+       int idade;
+       sexo;
+       public gato (string n) {
+           this.nome = n;
+       }
+       public void mia ( ) {
+           if (idade > = 2) {
+               system.out.print ("miauuu");
+           }
+           else{
+                system.out.print ("raaa");
+            }
+         }
+}
+
