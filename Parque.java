bfef8064f8b47684a354b1bd8ad662eca2a6501c
Giulia Rangel <49009091+giuliarangel@users.noreply.github.com>
2019-06-06 10:09:30 -0300

    Parque.java
    parte 2 do exercício gatos no dia 06/06

---
+++ Parque.java
@@ -0,0 +1,21 @@
+package gatos;
+
+public class Parque {
+		
+
+    public static void main (string [ ] args) {
+        parque puppy =  (  );
+        puppy.registro = "666";
+        puppy.nome = "Neve";
+        puppy.raca = "Persa";
+        puppy.sexo = "femea"; 
+        puppy.mia ();
+        gato.femea = new gato ();
+        femea.idade = 3;
+        femea.mia();
+     }
+}
+
+
+		
+
