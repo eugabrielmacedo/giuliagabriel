6040e9f99564cfc80a5d84d6d8563d2c359919a9
Gabriel Macedo <48810738+eugabrielmacedo@users.noreply.github.com>
2019-07-04 09:10:26 -0300

    endereço - pichau

---
+++ endereco.java
@@ -0,0 +1,14 @@
+package pichau;
+
+	public class endereco {
+	public static void main (string [ ] args) {
+        pichau.cep = "28735000";
+        pichau.cidade = "Quissama";
+        pichau.estado = "Rio de Janeiro";
+        pichau.pais = "Brasil";
+        pichau.rua = "Ivan Peixoto Dias";
+        pichau.numero = "33";
+        pichau.bairro = "Sitio";
+        pichau.complemento = "Proximo ao Britos";
+		}
+	}
