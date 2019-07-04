e7e4e808fc28d967d477170ffd03a68437eb21d0
Giulia Rangel <49009091+giuliarangel@users.noreply.github.com>
2019-07-04 09:26:47 -0300

    informações de endereço e cliente - pichau

---
+++ endereco-cliente.java
@@ -0,0 +1,20 @@
+package pichau;
+
+	public class endereco {
+	public static void main (string [ ] args) {
+		pichau.nome = "Giulia Rangel";
+		pichau.cpf = "454585698-98";
+		pichau.rg = "123548796-9";
+		pichau.sexo = "Feminino";
+		pichau.telefone = "22998563214";
+		pichau.celular = "40028922";
+        pichau.cep = "28735000";
+        pichau.cidade = "Quissama";
+        pichau.estado = "Rio de Janeiro";
+        pichau.pais = "Brasil";
+        pichau.rua = "Ivan Peixoto Dias";
+        pichau.numero = "33";
+        pichau.bairro = "Sitio";
+        pichau.complemento = "Proximo ao Britos";
+		}
+	}
