d868c3853667bf2efb72980da577e2436ce4d25e
Gabriel Macedo <48810738+eugabrielmacedo@users.noreply.github.com>
2019-07-04 09:04:37 -0300

    PICHAU - GABRIEL
    exercicio 1

---
+++ package pichau.java
@@ -0,0 +1,20 @@
+package pichau;
+
+public class pichau {
+	
+	
+	public class cliente {
+       cep;
+       string cidade;
+       estado;
+       pais;
+       rua;
+       int numero;
+       bairro;
+       complemento;
+       sexo;
+       public pichau (string n)
+            }
+      
+         }
+}
