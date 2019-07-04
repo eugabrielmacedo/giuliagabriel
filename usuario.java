9e02c6abe4cb484d5cb19a400a6c481dab419652
Giulia Rangel <49009091+giuliarangel@users.noreply.github.com>
2019-07-04 09:22:02 -0300

    usuario - pichau

---
+++ usuario.java
@@ -0,0 +1,13 @@
+public class pichau {
+	
+	
+	public class usuario {
+       nome;
+       string cpf;
+       rg;
+       sexo;
+       telefone;
+       int celular;
+       public pichau (string n)
+         }
+}
